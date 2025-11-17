65493f96b93df802eb5939598cb456ba77781393
liu-sunset <liulooklandscape@outlook.com>
2025-11-17 14:33:01 +0800

    初始化项目

---
+++ CarMainPF-project/src/main/java/peng/zhi/liu/carmainpfproject/CarMainPfProjectApplication.java
@@ -0,0 +1,13 @@
+package peng.zhi.liu.carmainpfproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMainPfProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarMainPfProjectApplication.class, args);
+    }
+
+}
